9c1fa3adb612e748de60b99b1db8105aee37f2c6
Adria Gual <u1933571@campus.udg.edu>
2021-01-21 13:12:33 +0100

    Login front finished

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -2,6 +2,7 @@ import 'dart:core';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:pawstic/components/textInput.dart';
 import "package:pawstic/globals.dart" as globals;
 import 'package:pawstic/pages/main/homeWrapper.dart';
@@ -46,105 +47,116 @@ class LoginState extends State<Login> {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-            child: Stack(children: [
+            child: Stack(alignment: Alignment.center, children: [
           Positioned(
-              bottom: 0,
+              bottom: 50,
               child: Image.asset(
-                'assets/images/onBoarding/onBoarding1.png',
-                width: 400.0,
+                'assets/images/login/login.png',
+                width: 200.0,
                 fit: BoxFit.cover,
               )),
           Padding(
-            padding: EdgeInsets.fromLTRB(30, 30, 30, 30),
-            child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: IconButton(
-                    padding: EdgeInsets.zero,
-                    constraints: BoxConstraints(),
-                    icon: Icon(
-                      FeatherIcons.arrowLeft,
-                      size: 30,
-                    ),
-                    onPressed: () {
-                      globals.selectedIndex = 0;
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeWrapper()),
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    child: Text('Iniciar sesión',
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.headline5),
-                  ),
-                ),
-                SizedBox(height: 15),
-                TextInput(name, 'Nombre', false),
-                SizedBox(height: 15),
-                TextInput(password, 'Contraseña', true),
-                SizedBox(height: 10),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Text(
-                    "¿has olvidado la contraseña?",
-                    style: TextStyle(
-                        fontFamily: 'PoppinsSemiBold',
-                        fontSize: 14.0,
-                        color: globals.bodyColor),
-                  ),
-                ),
-                SizedBox(height: 15),
-                SizedBox(
-                  width: 200.0,
-                  height: 60.0,
-                  child: FloatingActionButton.extended(
-                    onPressed: () {
-                      globals.selectedIndex = 0;
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeWrapper()),
-                      );
-                    },
-                    label: Text(
-                      "Entrar",
-                      style: TextStyle(
-                          fontFamily: 'PoppinsSemiBold',
-                          fontSize: 16.0,
-                          color: globals.whiteColor),
-                    ),
-                    backgroundColor: globals.primaryColor,
-                  ),
-                ),
-                SizedBox(height: 15),
-                Row(
-                  children: [
-                    Text(
-                      "¿Aún no tienes cuenta?",
-                      style: TextStyle(
-                          fontFamily: 'PoppinsRegular',
-                          fontSize: 16.0,
-                          color: globals.bodyColor),
+              padding: EdgeInsets.fromLTRB(30, 30, 30, 20),
+              child: Column(
+                children: [
+                  Row(children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        padding: EdgeInsets.zero,
+                        constraints: BoxConstraints(),
+                        icon: Icon(
+                          FeatherIcons.arrowLeft,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          globals.selectedIndex = 0;
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeWrapper()),
+                          );
+                        },
+                      ),
                     ),
                     Spacer(),
-                    Text(
-                      "Regístrate aquí",
+                    SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      height: 30.0,
+                    )
+                  ]),
+                  SizedBox(height: 20),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      child: Text('Iniciar sesión',
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context).textTheme.headline5),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextInput(name, 'Nombre', false),
+                  SizedBox(height: 20),
+                  TextInput(password, 'Contraseña', true),
+                  SizedBox(height: 15),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                      "¿has olvidado la contraseña?",
                       style: TextStyle(
                           fontFamily: 'PoppinsSemiBold',
-                          fontSize: 18.0,
-                          color: globals.primaryColor),
+                          fontSize: 14.0,
+                          color: globals.bodyColor),
                     ),
-                  ],
-                )
-              ],
-            ),
-          )
+                  ),
+                  SizedBox(height: 20),
+                  SizedBox(
+                    width: 200.0,
+                    height: 60.0,
+                    child: FloatingActionButton.extended(
+                      onPressed: () {
+                        globals.selectedIndex = 0;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeWrapper()),
+                        );
+                      },
+                      label: Text(
+                        "Entrar",
+                        style: TextStyle(
+                            fontFamily: 'PoppinsSemiBold',
+                            fontSize: 16.0,
+                            color: globals.whiteColor),
+                      ),
+                      backgroundColor: globals.primaryColor,
+                    ),
+                  ),
+                  Expanded(
+                      child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Row(
+                      children: [
+                        Text(
+                          "¿Aún no tienes cuenta?",
+                          style: TextStyle(
+                              fontFamily: 'PoppinsRegular',
+                              fontSize: 16.0,
+                              color: globals.bodyColor),
+                        ),
+                        Spacer(),
+                        Text(
+                          "Regístrate aquí",
+                          style: TextStyle(
+                              fontFamily: 'PoppinsSemiBold',
+                              fontSize: 18.0,
+                              color: globals.primaryColor),
+                        ),
+                      ],
+                    ),
+                  )),
+                ],
+              ))
         ])),
       ),
     );
